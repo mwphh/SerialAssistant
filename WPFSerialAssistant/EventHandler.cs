02c3eb5747f9c59979a703c818cae8e59283d41b
situyiran <situyiran1993@gmail.com>
2015-08-25 23:50:04 +0800

    状态栏时间自动更新

---
+++ WPFSerialAssistant/EventHandler.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFSerialAssistant
+{
+    public partial class MainWindow : Window
+    {
+        #region Event handler for menu items
+        private void saveSerialDataMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void saveConfigMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void loadConfigMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void exitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void serialSettingViewMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void autoSendDataSettingViewMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void serialCommunicationSettingViewMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void compactViewMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void aboutMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void helpMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region Event handler for buttons and so on.
+        private void openClosePortButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                if (ClosePort())
+                {
+                    openClosePortButton.Content = "打开";
+                }
+            }
+            else
+            {
+                if (OpenPort())
+                {
+                    openClosePortButton.Content = "关闭";
+                }
+            }
+        }
+
+        private void findPortButton_Click(object sender, RoutedEventArgs e)
+        {
+            FindPorts();
+        }
+
+        private void autoSendEnableCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void sendDataButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void recvCharacterRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void recvHexRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void showRecvDataDisableButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void saveRecvDataButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void clearRecvDataBoxButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void sendCharacterRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void sendHexRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void manualInputRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void loadFileRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void clearSendDataTextBox_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region Event handler for timers
+        private void ClockTimer_Tick(object sender, EventArgs e)
+        {
+            UpdateTimeDate();
+        }
+        #endregion
+    }
+}
---
+++ WPFSerialAssistant/SATimer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace WPFSerialAssistant
+{
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// 用于更新时间的定时器
+        /// </summary>
+        private DispatcherTimer clockTimer = new DispatcherTimer();
+
+        /// <summary>
+        /// 定时器初始化
+        /// </summary>
+        private void InitClockTimer()
+        {
+            clockTimer.Interval = new TimeSpan(0, 0, 1);
+            clockTimer.IsEnabled = true;
+            clockTimer.Tick += ClockTimer_Tick;
+            clockTimer.Start();
+        }
+    }
+}
